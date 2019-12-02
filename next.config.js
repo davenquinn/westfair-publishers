13aba61bff273715bb371e33092620f365c80adc
Daven Quinn <dev@davenquinn.com>
2019-12-02 04:23:49 -0500

    Changed pages to MDX

--- next.config.js
+++ next.config.js
@@ -1,10 +1,10 @@
 const withCSS = require('@zeit/next-css');
 const withStylus = require('@zeit/next-stylus');
-const withMDX = require('@next/mdx');
+const withMDX = require('@next/mdx')();
 const withCoffeescript = require('next-coffeescript');
 
-const cfg = {
-  cssModules: true
-};
+let cfg = withCoffeescript(
+  withCSS(withStylus(withMDX({cssModules: true}))));
+cfg.pageExtensions =  ['js', 'jsx', 'mdx','coffee'];
 
-module.exports = withCoffeescript(withCSS(withStylus(cfg)));
+module.exports = cfg;
