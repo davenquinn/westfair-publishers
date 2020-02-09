a8cb722992ccd4d844e4b71ccb4413acd66574c1
Daven Quinn <dev@davenquinn.com>
2020-02-09 00:06:59 -0600

    Changed config loading

--- next.config.js
+++ next.config.js
@@ -5,12 +5,11 @@ const withCoffeescript = require('next-coffeescript');
 
 // For production our env vars should be provided by ZEIT NOW
 if (process.env.NODE_ENV == 'development') {
-  const nowCfg = require('./now.json');
   require("dotenv").config()
 }
 
 // Assemble environment from process environment
-const keys = Object.keys(nowCfg.env)
+const keys = ["API_KEY", "DOMAIN", "EMAIL_TO", "EMAIL_FROM"]
 env = Object.fromEntries(keys.map(k=> [k, process.env[k]]));
 
 const baseCfg = {
