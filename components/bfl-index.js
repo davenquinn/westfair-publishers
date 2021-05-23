780cb33ef29afa929c8782d809db76254a9f462b
Daven Quinn <dev@davenquinn.com>
2021-05-23 18:18:20 -0500

    Index page is broadly functional

--- components/bfl-index.js
+++ components/bfl-index.js
@@ -2,27 +2,36 @@ import data from "./index-data";
 import h from "../styles";
 import { nest } from "d3-collection";
 import { SearchInput } from "evergreen-ui";
-import { useState, useRef, useMemo } from "react";
+import { useState, useRef, useMemo, useCallback } from "react";
 import Fuse from "fuse.js";
 
 const newData = data.map((d) => {
   return { firstName: d[0], lastName: d[1] };
 });
 
+const getSearchResults = (fuse, searchString) => {
+  let res = fuse.search(searchString);
+  if (searchString.length >= 3) {
+    res = res.filter((d) => d.score <= 0.15);
+  }
+  return res.map((d) => d.item);
+};
+
 function BFLIndexContent({ searchString = null }) {
   const fuse = useMemo(() => {
     return new Fuse(newData, {
       keys: [
         { name: "firstName", weight: 1 },
-        { name: "lastName", weight: 2 },
+        { name: "lastName", weight: 1 },
       ],
+      includeScore: true,
     });
   }, [newData]);
 
   const filteredData =
     searchString == null || searchString == ""
       ? newData
-      : fuse.search(searchString).map((d) => d.item);
+      : getSearchResults(fuse, searchString);
 
   const nested = nest()
     .key((d) => d.lastName)
@@ -34,9 +43,12 @@ function BFLIndexContent({ searchString = null }) {
       //const name = d.join(" ");
       return [
         h("dt", d.key),
-        d.values.map((v) => {
-          return h("dd", v.firstName);
-        }),
+        h(
+          "div.names",
+          d.values.map((v) => {
+            return h("dd", v.firstName);
+          })
+        ),
       ];
     })
   );
@@ -45,13 +57,16 @@ function BFLIndexContent({ searchString = null }) {
 function ByFirstLightIndex() {
   const [searchString, setSearchString] = useState(null);
   return h("div.bfl-index", [
-    h(SearchInput, {
-      onChange(e) {
-        console.log(e.target.value);
-        setSearchString(e.target.value);
-      },
-      searchString,
-    }),
+    h("div.bfl-index-header", [
+      h("h3", "Search the index: "),
+      h(SearchInput, {
+        className: "bfl-index-search",
+        onChange(e) {
+          setSearchString(e.target.value);
+        },
+        searchString,
+      }),
+    ]),
     h("div.bfl-index-content", [h(BFLIndexContent, { searchString })]),
   ]);
 }
